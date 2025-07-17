36bb1f6144d7e04f83268d8242312d970628b740
unknown <yeutaa>
2025-07-17 11:52:23 +0700

    week-3

---
+++ week-3/ex1.py
@@ -0,0 +1,10 @@
+decimal = input("input decimal here  ")    
+decimal = int(decimal)
+
+binary = bin(decimal)    
+hex = hex(decimal)
+oct = oct(decimal)
+
+print("this is binary ",binary)
+print("this is hex ",hex)
+print("this is oct ",oct)
